90b839f6f73f4fe0a2fcdaf7c10ef297c89a40df
Victor Castell <victorcoder@gmail.com>
2010-09-12 21:26:25 +0200

    templates path

--- recipes/sources.rb
+++ recipes/sources.rb
@@ -1,3 +1,3 @@
 package :ubuntu_sources do
-  transfer '../templates/sources.plist', '/etc/apt/sources.plist', :sudo => true
+  transfer File.dirname(__FILE__) + '../templates/sources.plist', '/etc/apt/sources.plist', :sudo => true
 end
