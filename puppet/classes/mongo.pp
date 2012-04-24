e9836105f817908bd8802216096b70eeee37620d
Victor Castell <victorcoder@gmail.com>
2012-04-24 17:55:31 +0200

    Mongo ensure latest

--- puppet/classes/mongo.pp
+++ puppet/classes/mongo.pp
@@ -52,7 +52,7 @@ class mongo {
       }
 
       package {"mongodb-10gen":
-        ensure => "2.0.3",
+        ensure => "latest",
         require => [Exec["update apt to find mongodb"], File["/etc/mongodb.conf"]]
       }
     }
