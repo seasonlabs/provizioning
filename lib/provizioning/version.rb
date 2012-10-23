01bcbbab5e61ac61d37e49bf7253374d9c13bdd4
Victor Castell <victorcoder@gmail.com>
2012-10-23 11:12:28 +0200

    Golang package name

--- lib/provizioning/version.rb
+++ lib/provizioning/version.rb
@@ -1,3 +1,3 @@
 module Provizioning
-  VERSION = "0.9.16"
+  VERSION = "0.9.17"
 end
--- puppet/modules/golang/manifests/init.pp
+++ puppet/modules/golang/manifests/init.pp
@@ -19,7 +19,7 @@ class golang {
       refreshonly => true,
     }
 
-    package {"golang":
+    package {"golang-stable":
       ensure => latest,
       require =>  Exec["update apt to find golang"],
     }
